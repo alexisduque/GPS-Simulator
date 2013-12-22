fd90ff550cdabcd4ae027678f51f6d414ab715a0
Alexis DUQUE <alexisd61@gmail.com>
2013-12-22 11:56:51 +0100

    multithread server implemented to test GPSimulator

--- src/gps/simulator/Server.java
+++ src/gps/simulator/Server.java
@@ -5,61 +5,121 @@
  * From Perl oPhone GPS Simulator
  * Author: Alexis DUQUE - alexisd61@gmail.com - 2013
  *
+ * Multhread Server listening on 42400 port. 10min Timeout. Build to test 
+ * Coded to test GPS Simulator app
+ *
  */
+
 package gps.simulator;
 
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
-/**
- *
- * @author Alex
- */
-public class Server extends Thread {
+public class Server implements Runnable {
 
-    final static int port = 9632;
-    private Socket socket;
+    protected int serverPort = 42400;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
 
-    public static void main(String[] args) {
-        try {
-            ServerSocket socketServeur = new ServerSocket(port);
-            System.out.println("**** Server Starting ****");
-            while (true) {
-                Socket socketClient = socketServeur.accept();
-                Server collect = new Server(socketClient);
-                collect.start();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Server(Socket socket) {
-        this.socket = socket;
+    public Server(int port) {
+        this.serverPort = port;
     }
 
     public void run() {
-        listening();
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+        System.out.println("Collect Server Started.");
+        while (!isStopped()) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if (isStopped()) {
+                    System.out.println("Server Stopped.");
+                    return;
+                }
+                throw new RuntimeException(
+                        "Error accepting client connection", e);
+            }
+            new Thread(
+                    new WorkerRunnable(
+                            clientSocket, "Collect Server")
+            ).start();
+        }
+        System.out.println("Server Stopped.");
     }
 
-    public void listening() {
-        try {
-            String message = "";
-            System.out.println("Connexion avec le client : " + socket.getInetAddress());
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintStream out = new PrintStream(socket.getOutputStream());
-            
-            message = in.readLine();
-            System.out.println(message);
-            
-            out.println("ACK" + message);
-            socket.close();
-            
+    public synchronized void stop() {
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
         } catch (IOException e) {
-            System.out.println("Oups ! Error receiving data");
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open port", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(42400);
+        new Thread(server).start();
+
+        try {
+            Thread.sleep(600 * 1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+        System.out.println("Stopping Server");
+        server.stop();
+    }
+
+    class WorkerRunnable implements Runnable {
+
+        protected Socket clientSocket = null;
+        protected String serverText = null;
+
+        public WorkerRunnable(Socket clientSocket, String serverText) {
+            this.clientSocket = clientSocket;
+            this.serverText = serverText;
+        }
+
+        public void run() {
+            try {
+                InputStream input = clientSocket.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(input));;
+                OutputStream output = clientSocket.getOutputStream();
+                String trame = in.readLine();
+                long time = System.currentTimeMillis();
+                output.write(("ACK "
+                        + this.serverText + " - "
+                        + time
+                        + "").getBytes());
+                output.close();
+                input.close();
+                System.out.println("Request processed: " + time);
+                System.out.println("Received: " + trame);
+            } catch (IOException e) {
+                //report exception somewhere.
+                e.printStackTrace();
+            }
         }
     }
 }
-
